ff8dc9dd2f8648823c630851cc94b18131e93daf
Cr3ativeCod3r <banaszekk123@gmail.com>
2026-06-10 09:46:28 +0200

    e-mail templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,12 +9,13 @@ urlpatterns = [
     # Auth
     path('', views.TrainerLoginView.as_view(), name='login'),
     path('rejestracja/', views.TrainerRegisterView.as_view(), name='register'),
+    path('rejestracja/sukces/', views.RegistrationSuccessView.as_view(), name='registration_success'),
     path('wyloguj/', auth_views.LogoutView.as_view(), name='logout'),
     
-    # Password Reset
     path('zapomnialem-hasla/', auth_views.PasswordResetView.as_view(
         template_name='accounts/password_reset.html',
-        email_template_name='accounts/password_reset_email.html',
+        email_template_name='emails/accounts/password_reset.txt',
+        html_email_template_name='emails/accounts/password_reset.html',
         success_url='/trenerzy/zapomnialem-hasla/wyslano/'
     ), name='password_reset'),
     path('zapomnialem-hasla/wyslano/', auth_views.PasswordResetDoneView.as_view(
